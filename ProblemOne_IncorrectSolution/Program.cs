63301b074a40ee8078531f793d1c045d1e8c78ae
Pranali21699 <87111027+Pranali21699@users.noreply.github.com>
2021-07-15 16:16:28 +0530

    Update Program.cs

--- ProblemOne_IncorrectSolution/Program.cs
+++ ProblemOne_IncorrectSolution/Program.cs
@@ -6,14 +6,12 @@ using System.Threading.Tasks;
 
 namespace ProblemOne_IncorrectSolution
 {
-    class Program
-    {
         static void Main(string[] args)
         {
             //1. The following code find the sum of all the numbers between 0 and 10000
             //2. The sum of the numbers should be for only numbers that are divisble by 3 or 5
             int result = 0;
-            for (int i = 0; i <= 1000; i++)
+            for (int i = 0; i <= 10000; i++)
             {
                 if (((i % 3) == 0) || ((i % 5) == 0))
                 {
